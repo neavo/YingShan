4333577f0dcb184fd116e99f480c19a8dba08549
Neavo <neavo7@gmail.com>
2012-11-05 10:56:44 +0800

    fix

--- assets/www/app.js
+++ assets/www/app.js
@@ -1,1 +1,58 @@
-Ext.application({name:"Project",requires:["Ext.MessageBox","Ext.field.Number","Ext.dataview.List","Ext.data.proxy.JsonP","Ext.carousel.Carousel",],models:["mainModel","childCategoryModel","expertModel","adModel",],stores:["mainStore","childCategoryStore","expertStore","adStore",],views:["widget.backBtn","widget.updateBtn","widget.aboutBtn","widget.homeBtn","widget.biggerBtn","widget.smallerBtn","widget.addBtn","widget.resetBtn","homeView","container.homeTop","container.homeMain","container.homeBottom","childCategory","container.childCategoryTop","container.childCategoryMain","container.childCategoryBottom","infoList","container.infoListTop","container.infoListMain","container.infoListBottom","infoDetail","container.infoDetailTop","container.infoDetailMain","container.infoDetailBottom","customHtml","container.customHtmlTop","container.customHtmlMain","container.customHtmlBottom","publishView","container.publishTop","container.publishMain","container.publishBottom","expertView","container.expertTop","container.expertMain","container.expertBottom","mainContainer",],controllers:["mainController","widget.backBtn","widget.updateBtn","widget.aboutBtn","widget.homeBtn","widget.biggerBtn","widget.smallerBtn","widget.addBtn","widget.resetBtn","homeView","container.homeMain","container.homeTop","childCategory","container.childCategoryMain","infoList","container.infoListMain","infoDetail","customHtml","publishView","expertView",],launch:function(){Ext.Viewport.add(Ext.create("Project.view.mainContainer"));},});
+Ext.application({
+
+	name : "Project",
+	
+	requires : [
+		"Ext.MessageBox",
+		"Ext.field.Number",
+		"Ext.dataview.List",
+		"Ext.data.proxy.JsonP",
+		"Ext.carousel.Carousel", 
+	],
+	
+	models : [
+		"mainModel",
+		"childCategoryModel",
+		"expertModel", 
+		"adModel", 
+	],
+	
+	stores : [
+		"mainStore",
+		"childCategoryStore",
+		"expertStore",
+		"adStore",
+	],
+	
+	views : [
+		"widget.backBtn", "widget.updateBtn", "widget.aboutBtn", "widget.homeBtn",
+		"widget.biggerBtn", "widget.smallerBtn", "widget.addBtn", "widget.resetBtn",
+		
+		"homeView", "container.homeTop", "container.homeMain", "container.homeBottom",
+		"childCategory", "container.childCategoryTop", "container.childCategoryMain", "container.childCategoryBottom",
+		"infoList", "container.infoListTop", "container.infoListMain", "container.infoListBottom",
+		"infoDetail", "container.infoDetailTop", "container.infoDetailMain", "container.infoDetailBottom",
+		"customHtml", "container.customHtmlTop", "container.customHtmlMain", "container.customHtmlBottom",
+		"publishView", "container.publishTop", "container.publishMain", "container.publishBottom",
+		"expertView", "container.expertTop", "container.expertMain", "container.expertBottom", "mainContainer",
+	],
+	
+	controllers : [
+		"mainController",
+		
+		"widget.backBtn", "widget.updateBtn", "widget.aboutBtn", "widget.homeBtn",
+		"widget.biggerBtn", "widget.smallerBtn", "widget.addBtn", "widget.resetBtn",
+		
+		"homeView", "container.homeMain", "container.homeTop",
+		"childCategory", "container.childCategoryMain",
+		"infoList", "container.infoListMain",
+		"infoDetail",
+		"customHtml",
+		"publishView",
+		"expertView",
+	],
+	
+	launch : function () {
+		Ext.Viewport.add(Ext.create("Project.view.mainContainer"));
+	},
+});
--- assets/www/app/view/container/homeMain.js
+++ assets/www/app/view/container/homeMain.js
@@ -1,1 +1,16 @@
-Ext.define("Project.view.container.homeMain",{extend:"Ext.Container",xtype:"homeMain",config:{layout:"vbox",items:[{id:"adCarousel",xtype:"carousel",cls:"adCarousel",},{id:"categoryContainer",layout:"vbox",flex:1,},],},});
+Ext.define("Project.view.container.homeMain", {
+	extend : "Ext.Container",
+	xtype : "homeMain",
+	config : {
+		layout : "vbox",
+		items : [{
+				id : "adCarousel",
+				xtype : "carousel",
+				cls : "adCarousel",
+			}, {
+				id : "categoryContainer",
+				layout : "vbox",
+				flex : 1,
+			}, ],
+	},
+});
