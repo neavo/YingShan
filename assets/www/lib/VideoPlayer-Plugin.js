555ddb91f024e0772542cde31c986fa7bf854c16
Neavo <neavo7@gmail.com>
2013-01-19 18:51:36 +0800

    1. 图片素材更新 2. Bug修正

--- assets/www/lib/VideoPlayer-Plugin.js
+++ assets/www/lib/VideoPlayer-Plugin.js
@@ -1,2 +1,17 @@
-function VideoPlayer(){};VideoPlayer.prototype.play=function(url){cordova.exec(null,null,"VideoPlayer","playVideo",[url]);};if(!window.plugins){window.plugins={};}
-if(!window.plugins.videoPlayer){window.plugins.videoPlayer=new VideoPlayer();}
+cordova.define("cordova/plugin/videoplayer",
+  function(require, exports, module) {
+    var exec = require("cordova/exec");
+    var VideoPlayer = function () {};
+
+    /**
+     * Starts the video player intent
+     *
+     * @param url           The url to play
+     */
+    VideoPlayer.prototype.play = function(url) {
+        exec(null, null, "VideoPlayer", "playVideo", [url]);
+    };
+
+    var videoPlayer = new VideoPlayer();
+    module.exports = videoPlayer;
+});
